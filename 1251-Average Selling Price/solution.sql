ded878f46d2af2d34deb64f711f22d5b9f366651
Bhuvana Chandrika Kothapalli <Bckothapally@gmail.com>
2024-01-23 11:03:17 -0600

    Update solution.sql

--- 1251-Average Selling Price/solution.sql
+++ 1251-Average Selling Price/solution.sql
@@ -1,4 +1,4 @@
-
+# Write your MySQL query statement below
 SELECT a.product_id, IFNULL(ROUND(SUM(a.price*b.units)/SUM(b.units),2),0) as average_price
 FROM Prices a
 LEFT JOIN UnitsSold b on a.product_id=b.product_id
