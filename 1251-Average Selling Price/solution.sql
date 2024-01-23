91b9418b81f7f9e52fefcd7a3fa8c66ec0198fcb
Bhuvana Chandrika Kothapalli <Bckothapally@gmail.com>
2024-01-23 10:50:13 -0600

    Create solution.sql

---
+++ 1251-Average Selling Price/solution.sql
@@ -0,0 +1,5 @@
+SELECT a.product_id, ROUND(SUM(a.price*b.units)/SUM(b.units),2) as average_price
+FROM Prices a
+LEFT JOIN UnitsSold b on a.product_id=b.product_id
+AND b.purchase_date BETWEEN a.start_date and a.end_date
+group by a.product_id
